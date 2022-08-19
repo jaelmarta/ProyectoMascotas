c5bd4c038cde808a41d14d861155ba3422dd0a22
Jael Marta <jachiis77@gmail.com>
2022-08-19 11:35:42 -0300

    solucion de problemas

--- js/proyectoMascotas.js
+++ js/proyectoMascotas.js
@@ -19,13 +19,13 @@ const catalogoMascotas = new CatalogoMascotas(mascotas);
 console.log("CATALOGO MASCOTAS", catalogoMascotas.mascotas);
 
 
+//Index
 
-
-const listarMascotas = (mascotasLista) => {
-  const mascotasDisponibles = document.getElementById("mascotasDisponibles");
-  mascotasDisponibles.innerHTML ="";
-  mascotasLista.forEach((mascotas) => {
-    mascotasDisponibles.innerHTML = `<div class="cad col-sm-12 col-md-6 col-lg-3 m-2" style="width: 15rem;">
+const listarMascotas = (mascotas) => {
+  const mascotasDisponibles = document.getElementById("mascotasEnAdopcion");
+  mascotas.innerHTML="";
+  mascotas.forEach((mascotas) => {
+    mascotas.innerHTML = `<div class="cad col-sm-12 col-md-6 col-lg-3 m-2" style="width: 15rem;">
                                       <div class="col">
                                         <div class="card">
                                         <img src=${mascotas.img} class="card-img-top" alt="${mascotas.nombre}">
@@ -46,6 +46,60 @@ const listarMascotas = (mascotasLista) => {
 listarMascotas(mascotas);
 
 
+
+
+//Adoptar
+let botonAdoptar=document.getElementById("botonAdoptar");
+
+const listado = document.getElementById("formularioSelector")
+mascotas.forEach (m=> {
+  const option = document.createElement("option")
+  option.value=m.id;
+  option.innerText=m.nombre;
+
+
+  listado.appendChild(option)
+  })
+  
+  document.getElementById("botonAdoptar").addEventListener("click", (mascotas)=> {
+  Swal.fire({
+    title: `Muchas gracias!`,
+    text: `Tu solicitud para adoptar a ${mascotas.nombre} fue enviada.`,
+    imageUrl:`${mascotas.img}`,
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: `${mascotas.nombre}`,
+  })
+  
+
+
+  let formularioNombre=document.getElementById("formularioNombre").value;
+  let formularioTelefono=document.getElementById("formularioTelefono").value;
+  let formularioCorreo=document.getElementById("formularioCorreo").value;
+  let formularioSelector=document.getElementById("formularioSelector").value;
+  let formularioPorQue=document.getElementById("formularioPorQue").value;
+  let formularioDondeYQuien=document.getElementById("formularioDondeYQuien").value;
+  let formularioCheck=document.getElementById("formularioCheck").value;
+
+
+  localStorage.setItem("formularioNombre", formularioNombre)
+  localStorage.setItem("formularioTelefono", formularioTelefono)
+  localStorage.setItem("formularioCorreo", formularioCorreo)
+  localStorage.setItem("formularioSelector", formularioSelector)
+  localStorage.setItem("formularioPorQue", formularioPorQue)
+  localStorage.setItem("formularioDondeYQuien", formularioDondeYQuien)
+  localStorage.setItem("formularioCheck", formularioCheck)
+
+})
+
+
+
+
+
+
+
+
+//Todas las secciones
   const mascotaABuscar = document.getElementById("searchButton");
   mascotaABuscar.addEventListener("click", ()=> {
     const buscadorMascotas=document.getElementById("buscador").value;
